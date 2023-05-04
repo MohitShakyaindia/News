5d6d50d6a18bcbe3df70760b63edf6590cf35d08
MohitShakyaindia <132532580+MohitShakyaindia@users.noreply.github.com>
2023-05-04 08:11:07 +0530

    Add files via upload

---
+++ precache-manifest.84419d0e76607e0db247835ba48cd0da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "58bad1d737d4e5c5d1b7",
+    "url": "/static/css/main.87bcd596.chunk.css"
+  },
+  {
+    "revision": "58bad1d737d4e5c5d1b7",
+    "url": "/static/js/main.571f1a3e.chunk.js"
+  },
+  {
+    "revision": "c2a4eb1306cf1afe55d9",
+    "url": "/static/js/runtime~main.f7f764ff.js"
+  },
+  {
+    "revision": "caec30dd5245bdbda698",
+    "url": "/static/js/2.91a07fd5.chunk.js"
+  },
+  {
+    "revision": "77da778a10b57eb72fd7",
+    "url": "/static/js/3.5d65379c.chunk.js"
+  },
+  {
+    "revision": "ce797770335edc42003a9c875280b426",
+    "url": "/static/media/loader.ce797770.gif"
+  },
+  {
+    "revision": "a242a21a246bc5c6855be50f8908ba97",
+    "url": "/index.html"
+  }
+];
